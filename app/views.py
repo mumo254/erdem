e4f5e4574bfa857dc0bbd23e1ba03fec24f7cbba
Job Karani <jobkarani084j@gmail.com>
2022-05-17 11:40:41 +0300

    updates blogs method

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from app.forms import ReplyForm
 from .models import *
 from django.shortcuts import render, redirect
 
@@ -10,9 +11,16 @@ def index(request):
 
 def blogs(request):
     posts = Blogs.objects.all()
-    reply = 
+    form = ReplyForm()
+    if request.method == 'POST':  
+        form = ReplyForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('blogs')
     print(posts)
     ctx= {
         "posts":posts,
+        "form":form,
     }
     return render (request, 'blog.html', ctx )
